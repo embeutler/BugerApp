4b6d8ec9eca915de9d27d48791c16f2661241dd5
emily <emily@emilyshow.com>
2019-11-21 12:21:47 -0800

    updates

--- models/burgers.js
+++ models/burgers.js
@@ -6,7 +6,7 @@ var burger= {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+
   create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
@@ -24,5 +24,4 @@ var burger= {
   }
 };
 
-// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
